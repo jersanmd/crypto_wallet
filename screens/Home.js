fb5aa906134e584efe130a88ac998e582d47c2f9
Jersan Balago <1stjersan@gmail.com>
2022-09-05 02:32:17 +0800

    CoinGecko Integration Done!

--- screens/Home.js
+++ screens/Home.js
@@ -3,17 +3,55 @@ import {
     View,
     Text
 } from 'react-native';
+import { SIZES, COLORS, FONTS, dummyData, icons } from '../constants';
 
 import MainLayout  from './MainLayout'
 
-const Home = () => {
+import { connect } from 'react-redux'
+import { getHoldings, getCoinMarket } from '../stores/market/marketActions'
+
+import { useFocusEffect } from '@react-navigation/native'
+
+
+const Home = ({getHoldings, getCoinMarket, myHoldings, coins}) => {
+
+    useFocusEffect(
+        React.useCallback(() => {
+            getHoldings(holdings = dummyData.holdings)
+            getCoinMarket()
+        }, [])
+    )
+
     return (
         <MainLayout>
-            <View>
+            <View
+                style={{
+                    backgroundColor: COLORS.white,
+                    height: '100%'
+                }}    
+            >
                 <Text>Home</Text>
             </View>
         </MainLayout>
     )
 }
 
-export default Home;
+function mapStateToProps(state) {
+    return {
+        myHoldings: state.marketReducer.holdings,
+        coins: state.marketReducer.coins
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        getHoldings: (holdings, currency, coinList, orderBy, sparkline, priceChangePerc, perPage, page) => {
+            return dispatch(getHoldings(holdings, currency, coinList, orderBy, sparkline, priceChangePerc, perPage, page))
+        },
+        getCoinMarket: (currency, coinList, orderBy, sparkline, priceChangePerc, perPage, page) => {
+            return dispatch(getCoinMarket(currency, coinList, orderBy, sparkline, priceChangePerc, perPage, page))
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
